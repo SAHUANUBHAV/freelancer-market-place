5d0ddad9a3fc9c45aa75dd5c3a753d885b096f33
Sharad Chauhan <sharadsingh6464@gmail.com>
2024-11-24 18:11:21 +0530

    Cors Config

--- api/server.js
+++ api/server.js
@@ -9,9 +9,17 @@ import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 const app = express();
 dotenv.config();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 
 const connect = async () => {
   try {
