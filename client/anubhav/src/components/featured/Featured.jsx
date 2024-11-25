a4c4b4ec391e15a6163fc6668066beb71434087e
Anubhav Sahu <anubhavsahu0103@gmail.com>
2024-11-25 15:08:01 +0530

    new changes

--- client/anubhav/src/components/featured/Featured.jsx
+++ client/anubhav/src/components/featured/Featured.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
 
 function Featured() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/gigs?search=${input}`);
+  };
   return (
     <div className="featured">
       <div className="container">
         <div className="left">
           <h1>
-          Discover the ideal freelance services for your business.
+            Find the perfect <span>freelance</span> services for your business
           </h1>
           <div className="search">
             <div className="searchInput">
               <img src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try "building LOGO"' />
+              <input
+                type="text"
+                placeholder='Try "building logo"'
+                onChange={(e) => setInput(e.target.value)}
+              />
             </div>
-            <button>Search</button>
+            <button onClick={handleSubmit}>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
-            <button>Graphics & Design</button>
-            <button>Photography</button>
+            <button>Web Design</button>
+            <button>WordPress</button>
             <button>Logo Design</button>
-            <button>Music & Audio</button>
+            <button>AI Services</button>
           </div>
         </div>
         <div className="right">
-          <img src="https://pngimg.com/uploads/student/student_PNG124.png" alt="ANUBHAV" />
+          <img src="https://pngimg.com/uploads/student/student_PNG124.png" alt="" />
         </div>
       </div>
     </div>
